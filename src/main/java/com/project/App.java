15cd0c4d1cb68a6a926fc086b190ad956ab4ed9e
RileyMwork <rileywmartin117@gmail.com>
2025-02-08 19:41:19 -0600

    Added entity classes

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,13 +1,24 @@
 package com.project;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import com.project.entity.APost;
+import com.project.entity.TvPost;
+import com.project.entity.User;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        User u = new User(1,"test@gmail.com","Test123");
+        System.out.println(u.toString());
+        TvPost t = new TvPost(1, 1, "TvPost message", Timestamp.valueOf(LocalDateTime.now()));
+        System.out.println(t.toString());
+        APost a = new APost(1, 1, 1, "APost message", Timestamp.valueOf(LocalDateTime.now()), "AAPL", "market", 
+                        "buy", true, 300.00, 1.00, "GTC", "adj398u9uihsdd", "aewur93u9u2903hsdds");
+        System.out.println(a.toString());
     }
 }
