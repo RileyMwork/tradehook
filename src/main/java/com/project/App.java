6e34dfd0ca65c5c8c63d5629010b6d6f4ffd90ec
RileyMwork <rileywmartin117@gmail.com>
2025-02-10 22:53:44 -0600

    UserDAO completed

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,24 +1,27 @@
 package com.project;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-
+import com.project.dao.DatabaseConnector;
+import com.project.dao.UserDAO;
 import com.project.entity.APost;
 import com.project.entity.TvPost;
 import com.project.entity.User;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) {
-        User u = new User(1,"test@gmail.com","Test123");
-        System.out.println(u.toString());
-        TvPost t = new TvPost(1, 1, "TvPost message", Timestamp.valueOf(LocalDateTime.now()));
-        System.out.println(t.toString());
-        APost a = new APost(1, 1, 1, "APost message", Timestamp.valueOf(LocalDateTime.now()), "AAPL", "market", 
-                        "buy", true, 300.00, 1.00, "GTC", "adj398u9uihsdd", "aewur93u9u2903hsdds");
-        System.out.println(a.toString());
+
+        User user = new User(1,"test@gmail.com","Test123","kasjfhasdlfjldjl","afjdlfdskjajsdlfjdkj","lksjdfjasfkfjkjdsfjk");
+        // System.out.println(u.toString());
+        // TvPost t = new TvPost(1, 1, "TvPost message", Timestamp.valueOf(LocalDateTime.now()));
+        // System.out.println(t.toString());
+        // APost a = new APost(1, 1, 1, "APost message", Timestamp.valueOf(LocalDateTime.now()), "AAPL", "market", 
+        //                 "buy", true, 300.00, 1.00, "GTC", "adj398u9uihsdd", "aewur93u9u2903hsdds");
+        // System.out.println(a.toString());
+        UserDAO u = new UserDAO();
+        // u.deleteUserById(1);
+        // System.out.println(u.insertNewUser(user));
+        System.out.println(u.updateUserAlpacaApiKeys(1, "newalpadsfsadfca", "SEEEHHIEsdfaadafdasaHHTEHI"));
     }
 }
